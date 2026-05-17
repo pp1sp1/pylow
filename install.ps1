68a0607706b146cacb2441c74ddb70c3d5d6f3b8
pp1sp1 <120898588+pp1sp1@users.noreply.github.com>
2026-05-17 09:28:19 +0200

    Update installation message to English

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 # Pobiera install.py z Git i uruchamia go za pomocą Pythona
 $RepoUrl = "https://raw.githubusercontent.com/pp1sp1/pylow/main/install.py"
 
-Write-Host "🚀 Rozpoczynanie instalacji PyLow..." -ForegroundColor Cyan
+Write-Host "🚀 Starting PyLow installation..." -ForegroundColor Cyan
 Invoke-WebRequest -Uri $RepoUrl -OutFile "install.py"
 python install.py
 Remove-Item "install.py"
