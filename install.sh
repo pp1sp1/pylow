e96fe0e33f9d920373f3444290672c1665fb0022
pp1sp1 <120898588+pp1sp1@users.noreply.github.com>
2026-05-17 09:27:57 +0200

    Update installation script to use English messages

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Pobiera install.py z Git i uruchamia go za pomocą Pythona
 REPO_URL="https://raw.githubusercontent.com/pp1sp1/pylow/main/install.py"
 
-echo "🚀 Rozpoczynanie instalacji PyLow..."
+echo "🚀 Starting PyLow installation..."
 curl -sSL $REPO_URL -o install.py
 python3 install.py
 rm install.py
